e3d7abdd6a7c14f721eae7bb8e55776fd4c91911
Matthew Farrell <farrellm@alum.mit.edu>
2019-12-18 22:01:31 -0500

    clean up

--- src/Graphics/Scad/Gear.hs
+++ src/Graphics/Scad/Gear.hs
@@ -69,7 +69,7 @@ tooth i rPitch =
       c' = rotate2d (-alphaRef) c
       pitch = pi * module' i
       theta = pitch / rPitch / 2
-   in hull [smodule [children, rotate2d theta $ mirror (V2 0 1) children] c']
+   in smodule (hull [children, rotate2d theta $ mirror (V2 0 1) children]) c'
 
 gear ::
      (Member (Reader Facet) r, Member (State (Map SomeModel Text)) r)
@@ -80,9 +80,12 @@ gear i rPitch =
   let pitch = pi * module' i
       theta = pitch / rPitch
       t = tooth i rPitch
-   in circle (rPitch - dedendum i) <+>
-      smodule
-        [rotate2d (theta * n) children | n <- [0 .. 2 * rPitch / module' i - 1]]
+   in smodule
+        (union
+           (circle (rPitch - dedendum i) :
+            [ rotate2d (theta * n) children
+            | n <- [0 .. 2 * rPitch / module' i - 1]
+            ]))
         t
 
 planetary ::
@@ -93,56 +96,43 @@ planetary ::
   -> Sem r Form
 planetary i p height =
   let sun = gear i (rSun p)
+      parity = 0.5 * fromIntegral (round (2 * rPlanet p / module' i) `mod` 2 :: Int)
       i' = i {addendum = dedendum i, dedendum = addendum i}
+      rRing = rSun p + 2 * rPlanet p
       ring = circle (rOuter p) <-> gear i' rRing
-   in list
+      pitch = pi * module' i
+            -- beta: the angle of omega per tooth cycle
+      betaRing = pitch / rRing
+      betaSun = pitch / rSun p -- betaPlanet == betaSun since meshed
+      dRing_dOmega = 1 / betaRing
+      dSun_dOmega = 1 / betaSun
+      theta = pitch / rPlanet p
+      planet omega =
+        let (_ :: Int, phaseRing) = properFraction (omega / betaRing)
+            (_ :: Int, phasePlnt) = properFraction (parity - omega / betaSun)
+            phasePlnt' =
+              if phasePlnt < 0
+                then phasePlnt + 1
+                else phasePlnt
+            delta = (phasePlnt' - phaseRing) / (dRing_dOmega + dSun_dOmega)
+            omega' = omega + 1 * delta
+         in rotate' (V3 0 0 omega') .
+            translate (V3 (rPlanet p + rSun p) 0 0) .
+            herringbone (-1) (rPlanet p) .
+            rotate2d ((theta / 2) + (omega' * rSun p / rPlanet p)) $
+            children
+   in union
         (herringbone (-1) rRing ring :
          herringbone (1) (rSun p) sun :
          [ smodule
-             [ planet (tau / fromIntegral (nPlanet p) * fromIntegral n)
-             | n <- [0 .. (nPlanet p) - 1]
-             ]
+             (union
+                [ planet (tau / fromIntegral (nPlanet p) * fromIntegral n)
+                | n <- [0 .. nPlanet p - 1]
+                ])
              (mirror (V2 1 0) . offsetR (-planetOffset p) False $
               gear i (rPlanet p))
          ])
   where
-    rRing = rSun p + 2 * rPlanet p
-
-    parity :: Double
-    parity =
-      if round (2 * rPlanet p / module' i) `mod` 2 == (0 :: Int)
-        then 0
-        else 0.5
-
-    planet ::
-         ( Member (Reader Facet) r
-         , Member (State (Map SomeModel Text)) r
-         , Member (HasChildren 'Two) r
-         )
-      => Double
-      -> Sem r Form
-    planet omega =
-      let pitch = pi * module' i
-            -- beta: the angle of omega per tooth cycle
-          betaRing = pitch / rRing
-          betaSun = pitch / rSun p -- betaPlanet == betaSun since meshed
-          dRing_dOmega = 1 / betaRing
-          dSun_dOmega = 1 / betaSun
-          theta = pitch / rPlanet p
-          (_ :: Int, phaseRing) = properFraction (omega / betaRing)
-          (_ :: Int, phasePlnt) = properFraction (parity - omega / betaSun)
-          phasePlnt' =
-            if phasePlnt < 0
-              then phasePlnt + 1
-              else phasePlnt
-          delta = (1) * (phasePlnt' - phaseRing) / (dRing_dOmega + dSun_dOmega)
-          omega' = omega + 1 * delta
-       in rotate' (V3 0 0 omega') .
-          translate (V3 (rPlanet p + rSun p) 0 0) .
-          herringbone (-1) (rPlanet p) .
-          rotate2d ((theta / 2) + (omega' * rSun p / rPlanet p)) $
-          children
-
     herringbone ::
          (Member (Reader Facet) r, Member (State (Map SomeModel Text)) r)
       => Double
@@ -150,8 +140,8 @@ planetary i p height =
       -> Sem r Shape
       -> Sem r Form
     herringbone sgn r m =
-      let eps = 1e-6
+      let eps = 1e-5
           height' = height + eps
-       in smodule [children <+> mirror (V3 0 0 1) children] .
+       in smodule (children <+> mirror (V3 0 0 1) children) .
           translate (V3 0 0 (-eps / 2)) $
           linearExtrude (0.5 * height') False 10 (0.5 * sgn * height' / r) m
